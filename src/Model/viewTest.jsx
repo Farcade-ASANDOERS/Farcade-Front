dad3c586d0c91c379519dcab88ecdda78d66dcb6
4Bee <4bee.code@gmail.com>
2024-08-15 21:55:48 +0900

    🚩: 개발에 필요한 svg 이미지 Assets/image/main 폴더에 삽입

---
+++ src/Model/viewTest.jsx
@@ -0,0 +1,21 @@
+import logo from '../Assets/image/Type-B.svg';
+import {
+  AppLogoSpin,
+  AppContainer,
+  AppLogo,
+  AppHeader,
+  AboutSection,
+} from '../App.styled';
+
+const App = () => {
+  return (
+    <AppContainer>
+      <AppHeader>
+        <AppLogo src={logo} alt="logo" />
+        <AboutSection>Hello! I am Farcade!!</AboutSection>
+      </AppHeader>
+    </AppContainer>
+  );
+};
+
+export default App;
