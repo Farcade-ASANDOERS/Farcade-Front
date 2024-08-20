4589228d5ab97d532f36b3b8fea8d57252ab29aa
4Bee <4bee.code@gmail.com>
2024-08-20 18:00:06 +0900

    ♻️: node의 require방식을 사용해서 이미지를 module 디렉토리에서 관리할 수있게 생성

---
+++ src/Model/data/img/img.data.js
@@ -0,0 +1,14 @@
+const images = {
+  bannerImage: require('../../../Assets/image/main/banner-plane.svg').default,
+  // ../ Assets / image / main / banner - plane.svg
+  personA: require('../../../Assets/image/main/person-a.svg').default,
+  personB: require('../../../Assets/image/main/person-b.svg').default,
+  personC: require('../../../Assets/image/main/person-c.svg').default,
+  mainMessage: require('../../../Assets/image/main/main-message.svg').default,
+  mainSet: require('../../../Assets/image/main/main-set.svg').default,
+  arrowPolygon: require('../../../Assets/image/main/arrow-polygon-down.svg')
+    .default,
+  arrowDown: require('../../../Assets/image/main/arrow-down.svg').default,
+};
+
+export default images;
